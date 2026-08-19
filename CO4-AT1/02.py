b6a3eb6c8518745217bae8353748cd2d8818b625
PranithaReddy22 <192425044.simats@saveetha.com>
2026-08-19 11:22:52 +0530

    CO4 - AT1

--- CO4-AT1/02.py
+++ CO4-AT1/02.py
@@ -2,7 +2,7 @@ machines = {
     "M1": "Active",
     "M2": "Active",
     "M3": "Maintenance",
-    "M4": "Active"
+    "M4": "Active",
 }
 for m, status in machines.items():
     if status == "Active":
--- CO4-AT1/04.py
+++ CO4-AT1/04.py
@@ -1,14 +1,28 @@
 sentences = {
-    "Doctor prescribed medicine to patient":
-        {"Doctor": "Agent", "medicine": "Theme", "patient": "Recipient"},
-    "Patient reported severe headache":
-        {"Patient": "Experiencer", "headache": "Symptom"},
-    "Nurse monitored patient":
-        {"Nurse": "Agent", "patient": "Theme"},
-    "Medicine reduced blood pressure":
-       {"Medicine": "Cause", "blood pressure": "Theme"}
+    "Doctor prescribed medicine to patient": {
+        "Doctor": "Agent",
+        "medicine": "Theme",
+        "patient": "Recipient"
+    },
+
+    "Patient reported severe headache": {
+        "Patient": "Experiencer",
+        "headache": "Symptom"
+    },
+
+    "Nurse monitored patient": {
+        "Nurse": "Agent",
+        "patient": "Theme"
+    },
+
+    "Medicine reduced blood pressure": {
+        "Medicine": "Cause",
+        "blood pressure": "Theme"
+    }
 }
+
 for sentence, roles in sentences.items():
-    print("\n", sentence)
+    print("\nSentence:", sentence)
+
     for word, role in roles.items():
         print(word, "->", role)
